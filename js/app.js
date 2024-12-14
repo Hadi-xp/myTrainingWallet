0605f099a34e99370f29fd1b90aa05debc31b485
Hadixp <hadi.b.2002@gmail.com>
2024-12-14 21:19:42 +0330

    validation added

--- js/app.js
+++ js/app.js
@@ -17,19 +17,24 @@ if(totalIncome !== ''){
 const btn = document.querySelector('.btn-primary');
 btn.addEventListener('click',(e)=>{
     e.preventDefault();
-    const expense = document.querySelector('#expense')
-    const amount = document.querySelector('#amount')
-    const ul = document.querySelector('.list-group');
-    const li = document.createElement('li');
-    const span = document.createElement('span');
-    const span2 = document.createElement('span');
-    span.textContent = expense.value;
-    span2.textContent = amount.value;
-    li.appendChild(span);
-    li.appendChild(span2);
-    ul.appendChild(li);
-    expense.value = '';
-    amount.value = '';
+    const expense = document.querySelector('#expense');
+    const amount = document.querySelector('#amount');
+    if(expense.value == '' || amount.value == ''){
+        alert('Please Fill All Fields');
+    }else{
+        const ul = document.querySelector('.list-group');
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        const span2 = document.createElement('span');
+        span.textContent = expense.value;
+        span2.textContent = amount.value;
+        li.appendChild(span);
+        li.appendChild(span2);
+        ul.appendChild(li);
+        expense.value = '';
+        amount.value = '';
+    }
+  
    
 })
 
